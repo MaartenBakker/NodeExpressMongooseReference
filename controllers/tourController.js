71a82219e08bc0703beb6f21f157bc91d6037ecb
Maarten Bakker <maartenbakker@gmail.com>
2020-09-01 13:46:17 +0200

    implement first alias

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,5 +1,11 @@
 const Tour = require('../models/tourModel');
-// const { express } = require('express');
+
+exports.aliasTopTours = async (req, res, next) => {
+  req.query.sort = '-ratingsAverage price';
+  req.query.limit = '5';
+  req.query.fields = 'name,price,ratingsAverage,summary,difficulty';
+  next();
+};
 
 exports.getAllTours = async (req, res) => {
   try {
