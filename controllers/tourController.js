7e8d9f844c299d23f6542fe790d3d7e4509ad2ae
Maarten Bakker <maartenbakker@gmail.com>
2020-09-06 22:18:38 +0200

    added getMonthlyPlan aggregation

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -144,3 +144,62 @@ exports.getTourStats = async (req, res) => {
     });
   }
 };
+
+exports.getMonthlyPlan = async (req, res) => {
+  try {
+    const year = Number.parseInt(req.params.year, 10);
+
+    const plan = await Tour.aggregate([
+      {
+        $unwind: '$startDates',
+      },
+      {
+        $match: {
+          startDates: {
+            $gte: new Date(`${year}-01-01`),
+            $lte: new Date(`${year}-12-31`),
+          },
+        },
+      },
+      {
+        $group: {
+          _id: { $month: '$startDates' },
+          numTourStarts: { $sum: 1 },
+          tours: { $push: '$name' },
+        },
+      },
+      {
+        $addFields: {
+          month: '$_id',
+        },
+      },
+      {
+        $project: {
+          _id: 0,
+        },
+      },
+      {
+        $sort: {
+          numTourStarts: -1,
+          month: 1,
+        },
+      },
+      {
+        $limit: 12, // just for demo
+      },
+    ]);
+
+    res.status(200).json({
+      status: 'success',
+      results: plan.length,
+      data: {
+        plan,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: `Could not get monthy plan, error: ${err}`,
+    });
+  }
+};
