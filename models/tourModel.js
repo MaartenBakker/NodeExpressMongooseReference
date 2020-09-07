dd85795e62941e24675d23e26ab1bd444474d801
Maarten Bakker <maartenbakker@gmail.com>
2020-09-07 11:09:22 +0200

    Document middleware

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 const tourSchema = mongoose.Schema(
   {
@@ -8,6 +9,7 @@ const tourSchema = mongoose.Schema(
       unique: true,
       trim: true,
     },
+    slug: String,
     duration: {
       type: Number,
       required: [true, 'A tour must have a duration'],
@@ -65,6 +67,22 @@ tourSchema.virtual('durationWeeks').get(function () {
   // Don't use arrow function!
   return this.duration / 7;
 });
+
+// DOCUMENT MIDDLEWARE: Runs before .save() & .create(), NOT befor .insertMany
+tourSchema.pre('save', function (next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+// tourSchema.pre('save', function (next) {
+//   console.log('Will save document...');
+//   next();
+// });
+
+// tourSchema.post('save', function (doc, next) {
+//   console.log(doc);
+//   next();
+// });
 
 const Tour = mongoose.model('Tour', tourSchema);
 
